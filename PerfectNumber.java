13fb97f6a4911f1290e5b6652f6b2fa719f110ea
vinit <vinitkkulkarni@gmail.com>
2023-04-07 00:40:11 +0530

    Added: logic for perfect number

---
+++ PerfectNumber.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.Day6;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to check for PerfectNumber : ");
+        long data = sc.nextLong();
+        long sum = 0;
+
+        for(int i=1; i<=data/2; i++){
+            if(data%i == 0){
+                sum = sum + i;
+            }
+        }
+
+        if(sum == data){
+            System.out.println(data + " is perfect number");
+        }else {
+            System.out.println(data + " not perfect number");
+        }
+    }
+}
